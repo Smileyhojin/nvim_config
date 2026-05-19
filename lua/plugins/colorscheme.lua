326f83bbdf7cc0cf7151f1a3940e3d167848fc67
Smileyhojin <ic434hojin@gmail.com>
2026-05-19 09:57:24 +0000

    Changed colorscheme to OneDark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,19 @@
 return {
+
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000, -- Load before all other start plugins
+    opts = {
+      -- Style options: dark, darker, cool, deep, warm, warmer, light
+      style = "cool",
+    },
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "onedark",
     },
   },
 }
